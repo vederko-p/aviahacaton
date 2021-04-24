526a986a1e084183abb898b06fa11809f8fd5c57
vederko-p <ski6a_23@mail.ru>
2021-04-24 15:29:06 +0300

    Расчет весов

--- back/graph.py
+++ back/graph.py
@@ -5,7 +5,7 @@ import numpy as np
 
 vertex = pd.read_csv('b_1f_edges_coords_px.csv', header=None)
 vertex.columns = ['x', 'y']
-links = pd.read_csv('b_1f_links_old.csv', header=None)
+links = pd.read_csv('b_1f_links.csv', header=None)
 links.columns = ['i', 'j']
 
 
@@ -13,5 +13,11 @@ def euclidean_distance(x0, y0, x1, y1):
     return ((x1-x0)**2 + (y1-y0)**2)**0.5
 
 
-(links-1).to_csv('b_1f_links.csv', index=False, header=False)
-# print(.head(5))
+lv = links.values
+vv = vertex.values
+
+weights = np.array([])
+for i in lv:
+    weights = np.append(weights, euclidean_distance(vv[i[0]][0], vv[i[0]][1], vv[i[1]][0], vv[i[1]][1]))
+
+print(weights[:6])
