f37ac19a97410bc55b997f3def684f20264d2ec3
vederko-p <ski6a_23@mail.ru>
2021-04-24 19:57:50 +0300

    adjacent_list - список списков смежных вершин

--- back/graph/graph.py
+++ back/graph/graph.py
@@ -17,9 +17,23 @@ lv = links.values
 vv = vertex.values  # Координаты вершин. Порядок точек в файле представляет нумерацию вершин.
 # (на нулевой позиции: vv[0] кординаты первой вершины и так далее)
 
-weights = np.zeros((vertex.size, vertex.size))
+weights = np.zeros((vertex.shape[0], vertex.shape[0]))
 for i in lv:
     weights[i[0], i[1]] = euclidean_distance(vv[i[0]][0], vv[i[0]][1], vv[i[1]][0], vv[i[1]][1])
     # weights - треугольная матрица весов, все значения весов лежат в верхнем треугольнике (weights[i, j], i < j)
 
 # print(weights[:5, :5])
+
+
+def adjacent_list(vertex_list, edges_list):
+    # edges_list - lv
+    # vertex_list - vv
+    els = range(vertex_list.shape[0])
+    adj_vertexes = []
+    for i in els:
+        t = np.unique(np.append(edges_list[edges_list[:, 0] == i], edges_list[edges_list[:, 1] == i]))
+        adj_vertexes.append(list(t[t != i]))
+    return adj_vertexes
+
+
+print(adjacent_list(vv, lv)[42])
