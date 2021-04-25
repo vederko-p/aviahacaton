7b80ac67434075efe32fe6f4329367bc9d05be44
aleha2001 <47641412+aleha2001@users.noreply.github.com>
2021-04-25 09:34:29 +0300

    Update astar.py

--- back/graph/astar.py
+++ back/graph/astar.py
@@ -1,26 +1,27 @@
-def h(start,end):#эвристика - расстояние
-    return ((end[0]-start[0])**2 + (end[1]-start[1])**2)**0.5   
+def h(start,end):#эвристика - расстояние 
+    return ((end[0]-start[0])**2 + (end[1]-start[1])**2)**0.5 
+def w(start,end): # вес ребра между соседними вершинами - в данном случае совпадает с h (здесь можно использовать матрицу весов)
+    return ((end[0]-start[0])**2 + (end[1]-start[1])**2)**0.5 
 def a(start,end,graph):
     n = len(graph)
     closed=[]#рассмотренные вершины
     openl = [start]#текущие вершины
     froml = {x:n for x in graph.keys()}
-    g = {x:n**2 for x in graph.keys()}
-    f = {x:n**2 for x in graph.keys()}
+    g = {x:10**4 for x in graph.keys()}
+    f = {x:10**4 for x in graph.keys()}
     g[start]=0
     for v in graph[start]:
-        g[v] = 1
+        g[v] = w(start,v)
     f[start] = h(start,end)
     while openl!=[]:
         curr = minf(openl,f)
-        #print(curr)
         if curr == end:
             return decode(froml,start,end)
         openl.remove(curr)
         closed.append(curr)
         for neig in graph[curr]:
             if not(neig in closed):
-                temp_g = g[curr] + 1
+                temp_g = g[curr] + w(curr,neig)
                 if not(neig in openl) or temp_g < g[neig]:
                     froml[neig] =curr
                     g[neig] = temp_g
